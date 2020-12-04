0b347bdd2c046ab2ef5be8295c95003260197aa3
tungli <tun@mail.muni.cz>
2020-12-04 14:20:53 +0100

    bug fix

--- bootstrap.js
+++ bootstrap.js
@@ -52,20 +52,20 @@
 /******/ 	function promiseResolve() { return Promise.resolve(); }
 /******/
 /******/ 	var wasmImportObjects = {
-/******/ 		"./node_modules/@tungli/wasm-f5170_magnetic_mirror_ex1/wasm_magnetic_mirror_bg.wasm": function() {
+/******/ 		"../pkg/wasm_magnetic_mirror_bg.wasm": function() {
 /******/ 			return {
 /******/ 				"./wasm_magnetic_mirror_bg.js": {
 /******/ 					"__wbindgen_json_serialize": function(p0i32,p1i32) {
-/******/ 						return installedModules["./node_modules/@tungli/wasm-f5170_magnetic_mirror_ex1/wasm_magnetic_mirror_bg.js"].exports["__wbindgen_json_serialize"](p0i32,p1i32);
+/******/ 						return installedModules["../pkg/wasm_magnetic_mirror_bg.js"].exports["__wbindgen_json_serialize"](p0i32,p1i32);
 /******/ 					},
 /******/ 					"__wbg_log_0aa7c485b853c606": function(p0i32,p1i32) {
-/******/ 						return installedModules["./node_modules/@tungli/wasm-f5170_magnetic_mirror_ex1/wasm_magnetic_mirror_bg.js"].exports["__wbg_log_0aa7c485b853c606"](p0i32,p1i32);
+/******/ 						return installedModules["../pkg/wasm_magnetic_mirror_bg.js"].exports["__wbg_log_0aa7c485b853c606"](p0i32,p1i32);
 /******/ 					},
 /******/ 					"__wbindgen_json_parse": function(p0i32,p1i32) {
-/******/ 						return installedModules["./node_modules/@tungli/wasm-f5170_magnetic_mirror_ex1/wasm_magnetic_mirror_bg.js"].exports["__wbindgen_json_parse"](p0i32,p1i32);
+/******/ 						return installedModules["../pkg/wasm_magnetic_mirror_bg.js"].exports["__wbindgen_json_parse"](p0i32,p1i32);
 /******/ 					},
 /******/ 					"__wbindgen_throw": function(p0i32,p1i32) {
-/******/ 						return installedModules["./node_modules/@tungli/wasm-f5170_magnetic_mirror_ex1/wasm_magnetic_mirror_bg.js"].exports["__wbindgen_throw"](p0i32,p1i32);
+/******/ 						return installedModules["../pkg/wasm_magnetic_mirror_bg.js"].exports["__wbindgen_throw"](p0i32,p1i32);
 /******/ 					}
 /******/ 				}
 /******/ 			};
@@ -158,7 +158,7 @@
 /******/
 /******/ 		// Fetch + compile chunk loading for webassembly
 /******/
-/******/ 		var wasmModules = {"0":["./node_modules/@tungli/wasm-f5170_magnetic_mirror_ex1/wasm_magnetic_mirror_bg.wasm"]}[chunkId] || [];
+/******/ 		var wasmModules = {"0":["../pkg/wasm_magnetic_mirror_bg.wasm"]}[chunkId] || [];
 /******/
 /******/ 		wasmModules.forEach(function(wasmModuleId) {
 /******/ 			var installedWasmModuleData = installedWasmModules[wasmModuleId];
@@ -168,7 +168,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"./node_modules/@tungli/wasm-f5170_magnetic_mirror_ex1/wasm_magnetic_mirror_bg.wasm":"5ac9d8e035a13d7cdcef"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_magnetic_mirror_bg.wasm":"ea31251a333b22330427"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
@@ -269,7 +269,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("// A dependency graph that contains any wasm must all be imported\n// asynchronously. This `bootstrap.js` file does the single async import, so\n// that no one else needs to worry about it again.\nPromise.all(/*! import() */[__webpack_require__.e(0), __webpack_require__.e(1)]).then(__webpack_require__.bind(null, /*! ./index.js */ \"./index.js\"))\n  .catch(e => console.error(\"Error importing `index.js`:\", e));\n\n\n//# sourceURL=webpack:///./bootstrap.js?");
+eval("// A dependency graph that contains any wasm must all be imported\n// asynchronously. This `bootstrap.js` file does the single async import, so\n// that no one else needs to worry about it again.\n__webpack_require__.e(/*! import() */ 0).then(__webpack_require__.bind(null, /*! ./index.js */ \"./index.js\"))\n  .catch(e => console.error(\"Error importing `index.js`:\", e));\n\n\n//# sourceURL=webpack:///./bootstrap.js?");
 
 /***/ })
 
